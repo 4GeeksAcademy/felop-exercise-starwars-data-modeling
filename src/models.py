54279ec6fbf3da97e46d4468a88967c0a8b47bff
Felop2299 <pipe-220899@hotmail.com>
2025-01-26 17:46:47 +0000

    final

--- src/models.py
+++ src/models.py
@@ -14,6 +14,8 @@ class User(Base):
     email= Column(String (250),nullable=False,unique=True)
     firstname=Column(String (250),nullable=False)
     lastname=Column(String (250),nullable=False)
+    password = Column(String(250), nullable=False)
+    fecha_de_suscripcion = Column(String(250), nullable=True)
 
 
 
@@ -22,21 +24,29 @@ class Favoritos (Base):
     __tablename__= 'favoritos'
     id=Column (Integer,primary_key=True)
     character_id=Column(Integer,ForeignKey('character.id'))
-    planet_id_=Column(Integer,ForeignKey('planet.id'))
+    planet_id =Column(Integer,ForeignKey('planet.id'))
     user_id =Column(Integer,ForeignKey('user.id'))
     user=relationship(User)
+    planet = relationship("Planet")
+    character = relationship("Character")
 
 class Planet (Base):
     __tablename__='planet'
     id=Column (Integer,primary_key=True)
-    Planet=Column(Integer,primary_key=True)
     planet=relationship(Favoritos)
+    name = Column(String(250), nullable=False) 
+    climate = Column(String(250), nullable=True)  
+    terrain = Column(String(250), nullable=True)  
+    population = Column(Integer, nullable=True) 
 
 
 class character(Base):
     __tablename__='character'
     id=Column (Integer,primary_key=True)
-    Character=Column(Integer,primary_key=True)
+    name = Column(String(250), nullable=False)  
+    gender = Column(String(20), nullable=True) 
+    height = Column(Integer, nullable=True)  
+    mass = Column(Integer, nullable=True)  
     character=relationship(Favoritos)
 
     def to_dict(self):
